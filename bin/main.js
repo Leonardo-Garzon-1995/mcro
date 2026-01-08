41d5d59eee778803f4903dd28567fb44aba851b0
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-01-08 09:00:52 +0100

    add validation improvements, improve formatting for interactive mode

--- bin/main.js
+++ bin/main.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { runKeyboardMode } from '../lib/interactive.js'; 
 import { runTerminalMode } from '../lib/terminal_mode.js';
-import { displayHelp, colors } from '../lib/helpers.js';
+import { displayHelp, colors, loadMacros } from '../lib/helpers.js';
 
 const [, , command] = process.argv;
 
@@ -14,30 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const macrosPath = path.join(__dirname, "..", 'macros.json');
 
-if (!fs.existsSync(macrosPath)) {
-    console.log(`${colors.red}macros.json not found${colors.reset}`);
-    console.log(`You should copy ${colors.bgGray}macros.json.example${colors.reset} to macros.json`);
-    process.exit(1);
-}
-
 let rawMacros = loadMacros(macrosPath);
-
-function loadMacros(path) {
-    try {
-    return JSON.parse(fs.readFileSync(path, "utf8"));
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            console.log(`${colors.red}macros.json not found${colors.reset}`);
-            console.log(`You should copy ${colors.bgGray}macros.json.example${colors.reset} to macros.json`);
-        }
-        if (error instanceof SyntaxError) {
-            console.log(`${colors.red}macros.json invalid:${colors.reset}`, error.message);
-        }
-        process.exit(1);
-    }
-}
-
-
 const macrosByKey = Object.fromEntries(rawMacros.map(obj => [obj.key, obj]))
 const macrosByCommand = Object.fromEntries(rawMacros.map(obj => [obj.command, obj]))
 
